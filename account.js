168ab16431032b2aab389ddec57627eebc580b9e
ozo-vehe <johnnie.vehe@gmail.com>
2024-03-08 05:52:09 +0100

    zetachain

---
+++ account.js
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+// 🔑 Private key: cd4fcf72dec17f37338f9b368b264c27edf0b9f34ffc71d4d9fcb3903ec8dbf0
+// 🔐 Mnemonic phrase: seek soft easily slight level orient plunge govern jazz purchase total check
+// 😃 EVM address: 0x46c51164f97e0dE89D06F4C1B436fb82981FB916
+// 😃 Bitcoin address: tb1qx2htf9dl06gpllsr73r59yv552mdvq3ehnwsms
+// 😃 Bech32 address: zeta1gmz3ze8e0cx738gx7nqmgdhms2vplwgkzf2k70
+// 🔑 Using account: 0x46c51164f97e0dE89D06F4C1B436fb82981FB916
+
+// 🚀 Successfully deployed contract on ZetaChain.
+// 📜 Contract address: 0xf452d1e6f988f7A7414Ed76B9348221f82440F18
+// 🌍 Explorer: https://athens3.explorer.zetachain.com/address/0xf452d1e6f988f7A7414Ed76B9348221f82440F18
